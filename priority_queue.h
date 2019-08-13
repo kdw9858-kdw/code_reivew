a8fb0384caf4fff531332c8546ae9d9dc219224f
kdw9858-kdw <53922931+kdw9858-kdw@users.noreply.github.com>
2019-08-13 15:40:39 +0900

    Apply a result of code review.
    Apply a result of code review.

--- priority_queue.h
+++ priority_queue.h
@@ -1,15 +1,15 @@
 #include <queue>
 
-template <typename T> 
-class templatePriorityQueue {
-	private:
-		std::priority_queue<T> storage;
-	public:
-		templatePriorityQueue();
-		~templatePriorityQueue();
-		bool empty() const;
-		const T& top() const;
-		int size();
-		void push(const T&);
-		void pop();	
+template <typename T>
+class TemplatePriorityQueue {
+ public:
+  templatePriorityQueue();
+  ~templatePriorityQueue();
+  bool empty() const;
+  const T& top() const;
+  int size();
+  void push(const T&);
+  void pop();
+ private:
+  std::priority_queue<T> storage;
 };
